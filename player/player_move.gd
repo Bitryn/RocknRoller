79c50da0500c9b852899caff187c9031499d9655
CwikUU <hujowe69@gmail.com>
2026-07-20 22:21:40 +0200

    teren, gracz
    wykorzystanie pluginow na teren w 3D, zrobienie jakis gorek/dolkow
    Sprawdzenie ruchu gracza do terenu, po czesci przygotowanie do tego ruchu lewo, prawo

---
+++ player/player_move.gd
@@ -0,0 +1,35 @@
+extends CharacterBody3D
+
+
+const speed_array = [10,25]
+const JUMP_VELOCITY = 4.5
+
+var speed = 10
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# sprint
+	if Input.is_action_pressed("sprint"):
+		speed = speed_array[1]
+	else:
+		speed = speed_array[0]
+		
+
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
