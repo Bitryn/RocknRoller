6fda52b6768fad7dba60b7f6146870b8cbb721aa
CwikUU <hujowe69@gmail.com>
2026-07-21 22:02:51 +0200

    chodzenie
    Zmienienie troche MainCharacter
    scena juz dziala
    popracowanie troche nad ruchem gracza ( wchodzenie i schodzenie z gorki)
    skok w jednym kierunku z brakiem mozliwosci zatrzymania/zmiany kierunku skoku

--- mainCharacter/main_character.gd
+++ mainCharacter/main_character.gd
@@ -1,35 +1,45 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const speed_array = [10,25]
 const JUMP_VELOCITY = 4.5
 
+var speed = 10
 var input_dir
+@export var Player_Sprite: AnimatedSprite3D
 
 func _process(delta: float) -> void:
 	GlobVar.PlayerPos = position
-	input_dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	#input_dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if input_dir.x >0.4:
-		$spr.flip_h = false
-	elif input_dir.x <-0.4:
-		$spr.flip_h = true
+	if Input.is_action_just_pressed("move_right") :
+		Player_Sprite.flip_h = false
+	elif Input.is_action_just_pressed("move_left") :
+		Player_Sprite.flip_h = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		velocity.y += get_gravity().y * delta
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	# sprint
+	if Input.is_action_pressed("sprint"):
+		speed = speed_array[1]
+	else:
+		speed = speed_array[0]
+		
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	#input_dir := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	
-	velocity.x = input_dir*delta *400
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction and is_on_floor():
+		velocity.x = direction.x * speed
+	elif is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, speed)
 	
 	move_and_slide()
--- player/player_move.gd
+++ player/player_move.gd
@@ -6,7 +6,6 @@ const JUMP_VELOCITY = 4.5
 
 var speed = 10
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
