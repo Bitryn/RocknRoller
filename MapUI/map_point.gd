aad15c99a67ef2658f3402c72164c4b0b4a9323c
Biteryn <magistr.bodia@gmail.com>
2026-07-30 13:41:39 +0200

    +Cannon model and started map ui system

---
+++ MapUI/map_point.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export_category("Viable Points")
+@export var Point0 := Node2D
+@export var Point1 := Node2D
+@export var Point2 := Node2D
+@export var Point3 := Node2D
+@export var Point4 := Node2D
+@export var Point5 := Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
