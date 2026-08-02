f5abd9bd233841bf5a60983408a130646c90d54a
CwikUU <hujowe69@gmail.com>
2026-08-02 23:30:25 +0200

    ramp, break
    Naprawa canon na lepsze
    
    WIP rampa automatyczne otwieranie/zamykanie
    
    Wyhamowanie jak ci Roller ucieknie

--- Roller/Canon/canon.gd
+++ Roller/Canon/canon.gd
@@ -5,5 +5,5 @@ var rotationZ = rotation.z
 var bullet_spawn
 
 func _physics_process(delta: float) -> void:
-	bullet_spawn = $Node3D.global_transform
+	bullet_spawn = $BulletSpawn.global_transform
 	rotation.z = rotationZ * -1 * 0.1
