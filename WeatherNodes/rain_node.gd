c7b43bb6eafc7554896a428f3bb7526060126e35
CwikUU <hujowe69@gmail.com>
2026-08-24 22:00:27 +0200

    Revert "Merge branch 'bit' into tworzenie"
    This reverts commit 87a9cb82001174d21bdb0a110856562ef8c77a02, reversing
    changes made to be2ad1ee55765a83e8fa587a1380d2a9b8147f97.

--- WeatherNodes/rain_node.gd
+++ WeatherNodes/rain_node.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	if Lightning:
 		$Timer.start()
 	
-	$Drops.process_material.direction = RainVec
+	$GPUParticles3D.process_material.direction = RainVec
 	pass # Replace with function body.
 
 
